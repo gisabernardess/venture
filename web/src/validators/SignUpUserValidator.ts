327c39e87b9b1c487de3ecd44590c59d784e5237
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-10-04 21:44:09 -0300

    qa fixes

--- web/src/validators/SignUpUserValidator.ts
+++ web/src/validators/SignUpUserValidator.ts
@@ -3,9 +3,10 @@ import * as yup from 'yup';
 export const signUpFormSchema = yup.object().shape({
   name: yup.string().required('Name is required'),
   email: yup.string().required('Email is required').email('Invalid email'),
-  password: yup.string().required('Password is required'),
+  password: yup.string().required('Password is required').min(8),
   password_confirmation: yup
     .string()
+    .min(8)
     .when('password', (password, field) =>
       password ? field.required('Passwords do not match') : field,
     ),
